b427969a5cdca758dfcaec24d4a0d24fb7d0fd3a
mermaidkids <therake463@gmail.com>
2023-11-26 06:17:01 +0700

    contoh href image

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="image/GaweWorld.png" alt="Logo" width="48" height="36" class="d-inline-block align-text-top">
-            <span class="text-danger font-weight-bold">Gaωe World</span>
-        </a>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link text-danger font-weight-bold" href="#">Kembali</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="<?= base_url('assets/'); ?>img/GaweWorld.png" alt="Logo" width="48" height="36" class="d-inline-block align-text-top">
+                <span class="text-danger font-weight-bold">Gaωe World</span>
+            </a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-danger font-weight-bold" href="#">Kembali</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <!-- Content Container -->
     <div class="container mt-5">
-        
+
 
         <div class="row">
             <!-- Carousel Column -->
@@ -57,24 +59,24 @@
                         </div>
                         <div class="text-center">
                             <button type="submit" class="btn btn-primary w-100">Masuk</button>
-                        </div>                        
+                        </div>
                     </form>
                     <br>
                     <div class="mt-3 text-center">
                         <p class="mb-0">Belum punya akun?</p>
                         <a href="#" class="btn btn-warning btn-sm">Daftar</a>
-                    </div>                    
+                    </div>
                 </div>
             </div>
 
-                <!-- Navbar untuk "Butuh Bantuan" -->
-<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-bottom">
-    <div class="container">
-        <center class="mx-auto">
-            <div class="text-center">
-                <p>Butuh Bantuan</p>
-                <a href="#" class="btn btn-warning btn-sm">Lihat Pusat Bantuan</a>
-            </div>
-        </center>
-    </div>
-</nav>
+            <!-- Navbar untuk "Butuh Bantuan" -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-bottom">
+                <div class="container">
+                    <center class="mx-auto">
+                        <div class="text-center">
+                            <p>Butuh Bantuan</p>
+                            <a href="#" class="btn btn-warning btn-sm">Lihat Pusat Bantuan</a>
+                        </div>
+                    </center>
+                </div>
+            </nav>
